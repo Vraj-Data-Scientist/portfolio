d3bb6f6db55077a71efe5b5b87ed2e523287dac9
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2025-05-24 06:31:46 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   AiFillStar,
   AiOutlineHome,
